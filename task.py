a604ac4cdfc9c2b7b1b39605ac04149ad620e2fb
Sangam-mishra2003 <131407364+Sangam-mishra2003@users.noreply.github.com>
2025-11-11 19:31:26 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,4 @@
+print("Welcome to the brand name generator!")
+city_name = input("What is your city name where you grew up")
+pet_name = input("your pet name")
+print ("your brand name could be "+ city_name +" " + pet_name + "!")
